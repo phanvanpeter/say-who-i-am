900fb7fd2735c7640d8476ea4cce6fa90eff0039
Peter Phanvan <pfunnyyy@gmail.com>
2022-07-06 08:42:11 +0200

    refactoring main: separated functions for initializing and starting server and waiting for it's termination

--- people/main.go
+++ people/main.go
@@ -12,20 +12,35 @@ import (
 
 var bindAddr = ":9090"
 
+var logger hclog.Logger
+
 func main() {
-	l := hclog.Default()
-	err := run(&l)
+	logger = hclog.Default()
+	err := run()
 	if err != nil {
-		l.Error("Error shutting down the server", err)
+		logger.Error("Error shutting down the server", err)
 		os.Exit(1)
 	}
 }
 
-func run(l *hclog.Logger) error {
-	logger := *l
+// run starts the server for people API
+func run() error {
 	r := mux.NewRouter()
 
-	srv := http.Server{
+	srv := newServer(r)
+	startServer(srv)
+
+	waitForSignal(logger)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	return srv.Shutdown(ctx)
+}
+
+// newServer initializes and returns a new server
+func newServer(r *mux.Router) *http.Server {
+	return &http.Server{
 		Addr:         bindAddr,
 		Handler:      r,
 		ErrorLog:     logger.StandardLogger(&hclog.StandardLoggerOptions{}),
@@ -33,7 +48,11 @@ func run(l *hclog.Logger) error {
 		WriteTimeout: 10 * time.Second,
 		IdleTimeout:  2 * time.Minute,
 	}
+}
 
+// startServer listens on the TCP network address and then calls Serve to handle requests on incoming connections.
+// A new goroutine is initialized for this process.
+func startServer(srv *http.Server) {
 	go func() {
 		err := srv.ListenAndServe()
 		if err != nil {
@@ -41,16 +60,15 @@ func run(l *hclog.Logger) error {
 			os.Exit(1)
 		}
 	}()
+}
 
+// waitForSignal waits for the os.Interrupt or os.Kill signal. Until then, the main goroutine is blocked.
+// This prevents from the immediate shutdown of the server.
+func waitForSignal(logger hclog.Logger) {
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, os.Kill)
 
 	sig := <-sigChan
 	logger.Info("Gracefully shutting down the server:", sig)
-
-	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-	defer cancel()
-
-	return srv.Shutdown(ctx)
 }
