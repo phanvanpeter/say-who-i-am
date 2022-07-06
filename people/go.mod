784099467f50d0f47686ab0b191622d73738bec3
Peter Phanvan <pfunnyyy@gmail.com>
2022-07-06 07:57:50 +0200

    initializing and configuring the server

--- people/go.mod
+++ people/go.mod
@@ -1,3 +1,12 @@
 module github.com/phanvanpeter/say-who-i-am/people
 
 go 1.18
+
+require (
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/go-hclog v1.2.1 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+)
--- people/main.go
+++ people/main.go
@@ -1,7 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"github.com/hashicorp/go-hclog"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var bindAddr = ":9090"
 
 func main() {
-	fmt.Println("Hi, People!")
+	l := hclog.Default()
+	err := run(&l)
+	if err != nil {
+		l.Error("Error shutting down the server", err)
+		os.Exit(1)
+	}
+}
+
+func run(l *hclog.Logger) error {
+	logger := *l
+	r := mux.NewRouter()
+
+	srv := http.Server{
+		Addr:         bindAddr,
+		Handler:      r,
+		ErrorLog:     logger.StandardLogger(&hclog.StandardLoggerOptions{}),
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  2 * time.Minute,
+	}
+
+	go func() {
+		err := srv.ListenAndServe()
+		if err != nil {
+			logger.Error("Server interrupted:", err)
+			os.Exit(1)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	logger.Info("Gracefully shutting down the server:", sig)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	return srv.Shutdown(ctx)
 }
